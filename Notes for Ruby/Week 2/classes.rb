f446ca922e5b3b6c3302bbfa67b782db6cb2985d
joshua <yoshiama12@gmail.com>
2016-07-17 18:12:22 -0400

    attr_accessor lesson

--- Notes for Ruby/Week 2/classes.rb
+++ Notes for Ruby/Week 2/classes.rb
@@ -31,3 +31,16 @@ person1 = Person.new("Joe", 14)
 puts person1.name
 person1.name = "Mike"
 puts person1.name
+
+class Person
+  attr_accessor :name, :age
+end
+
+person1 = Person.new
+p person1.name
+person1.name = "Mike"
+person1.age = 15
+puts person1.name
+puts person1.age
+person1.age = "fifteen"
+puts person1.age
