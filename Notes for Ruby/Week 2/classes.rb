854813d935fe0780c9d675f8bff8d2322719ff4f
joshua <yoshiama12@gmail.com>
2016-07-17 18:22:43 -0400

    learning about self

--- Notes for Ruby/Week 2/classes.rb
+++ Notes for Ruby/Week 2/classes.rb
@@ -44,3 +44,22 @@ puts person1.name
 puts person1.age
 person1.age = "fifteen"
 puts person1.age
+
+class Person
+  attr_reader :age
+  attr_accessor :name
+
+  def initialize (name, ageVar)
+    @name = name
+    self.age = ageVar
+    puts age
+  end
+  def age= (new_age)
+    @age = new_age unless new_age > 120
+  end
+end
+
+person1 = Person.new("Kim", 13)
+puts "My age is #{person1.age}"
+person1.age = 14
+puts "My age is #{person1.age}"
