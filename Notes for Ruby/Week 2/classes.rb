d153fad1ad48f418f75173900c33ded2ce294cc2
joshua <yoshiama12@gmail.com>
2016-07-17 18:08:28 -0400

    more class notes

--- Notes for Ruby/Week 2/classes.rb
+++ Notes for Ruby/Week 2/classes.rb
@@ -13,3 +13,21 @@ person1 = Person.new("Joe", 14)
 p person1.instance_variables
 puts person1.get_info
 p person1.instance_variables
+
+class Person
+  def initialize (name, age)
+    @name = name
+    @age = age
+  end
+  def name
+    @name
+  end
+  def name= (new_name)
+    @name = new_name
+  end
+end
+
+person1 = Person.new("Joe", 14)
+puts person1.name
+person1.name = "Mike"
+puts person1.name
