d912cccdc04f17752b229645484b02bf32602d37
joshua <yoshiama12@gmail.com>
2016-07-18 02:14:02 -0400

    scope lessons

--- Notes for Ruby/Week 2/scope.rb
+++ Notes for Ruby/Week 2/scope.rb
@@ -14,3 +14,40 @@ obj.my_method
 
 p local_variables
 p self
+
+v1 = "outside"
+
+class MyClass
+  def my_method
+  v1 = "inside"
+  p v1
+  p local_variables
+  end
+end
+
+p v1
+obj = MyClass.new
+obj.my_method
+
+p local_variables
+p self
+
+module Test
+  PI = 3.14
+  class Test2
+    def what_is_pi
+      puts PI
+    end
+  end
+end
+Test::Test2.new.what_is_pi
+
+module MyModule
+  MyConstant = 'Outer Constant'
+  class MyClass
+    puts MyConstant
+    MyConstant = 'Inner Constant'
+    puts MyConstant
+  end
+  puts MyConstant
+end
